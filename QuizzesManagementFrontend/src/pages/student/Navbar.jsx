37f3e509e8922c1f92b2892447f8bd17e2c490b2
BigcBow <s6404062610481@email.kmutnb.ac.th>
2024-11-02 04:06:25 +0700

    Create Navbar Left

--- QuizzesManagementFrontend/src/pages/student/Navbar.jsx
+++ QuizzesManagementFrontend/src/pages/student/Navbar.jsx
@@ -1,43 +1,41 @@
 import '../../styles/student/student.css';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse, faSquarePollVertical, faFilePen, faFile, faFaceSmile, faArrowRightFromBracket} from '@fortawesome/free-solid-svg-icons'
 
 export default function Navbar() {
     return (
-        // <div className="navbar">
-        //     <div className="navbar_left">
-        //         <a href="#" className="cs">CS</a>
-        //         <a href="#">Quizzes Management</a>
-        //     </div>
-        //     <div className="navbar_right">
-        //         <div className="navbar_right_icon">
-        //             <i className="fa-regular fa-circle-user"></i>
-        //         </div>
-        //         <div className="navbar_right_box">
-        //             <p className="username">Firstname Lastname</p>
-        //             <p className="student">Student</p>
-        //         </div>
-        //         <div className="navbar_right_icon2">
-        //             <i className="fa-solid fa-bell"></i>
-        //         </div>
-        //     </div>
-        // </div>
     <div className="main">
         <div className="left">
             <div className="left_top">
-                <Link to='/home_student' className='icon_cs'>CS</Link>
-                <Link to='/home_student' className='quiz'>Quizzes Management</Link>
-            </div>
-            <div className="left_main">
-                <div className="left_main_top">
-                    Menu
+                <div className="left_top_top">
+                    <Link to='/home_student' className='icon_cs'>CS</Link>
+                    <Link to='/home_student' className='quiz'>Quizzes Management</Link>
                 </div>
+                <div className="left_top_main">
+                    <Link to='/#' className='mune'><FontAwesomeIcon icon={faHouse} className='icon_menu'/>Home</Link>
+                    <Link to='/#' className='mune'><FontAwesomeIcon icon={faSquarePollVertical} className='icon_menu' />Dashboard</Link>
+                    <Link to='/#' className='mune'><FontAwesomeIcon icon={faFile} className='icon_menu'/>Create Test Set</Link>
+                    <Link to='/#' className='mune'><FontAwesomeIcon icon={faFilePen} className='icon_menu'/>Practice</Link>
+                </div>
+            </div>
+            <div className="left_tail">
                 <div className="left_main_tail">
-                    Logout
+                    <Link to='/#' className='mune'><FontAwesomeIcon icon={faFaceSmile} className='icon_menu'/>Home</Link>
+                    <Link to='/#' className='mune'><FontAwesomeIcon icon={faArrowRightFromBracket} className='icon_menu' />Dashboard</Link>
                 </div>
             </div>
         </div>
         <div className="right">
-            da
+            <div className="right_top">
+                <div className="right_top_free"></div>
+                <div className="right_top_tail">
+                    
+                </div>
+            </div>
+            <div className="right_main">
+
+            </div>
         </div>
     </div>
     )
