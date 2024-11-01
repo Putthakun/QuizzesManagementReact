0da09598faf16f61d181c200654ccfb9250e5805
BigcBow <s6404062610481@email.kmutnb.ac.th>
2024-11-02 06:37:46 +0700

    Test many box and add overflow css

--- QuizzesManagementFrontend/src/pages/student/Subject_student.jsx
+++ QuizzesManagementFrontend/src/pages/student/Subject_student.jsx
@@ -17,8 +17,91 @@ function Subject_student() {
                             <div className="main_right_box_subject_head">
                                 <div className="main_right_box_subject_head_circle"></div>
                                 <div className="main_right_box_subject_head_name">
-                                    <p>Math</p>
+                                    <h3>Math</h3>
                                     <p></p>
+                                </div>
+                            </div>
+                            <div className="main_right_box_subject_main">
+                                <div className="main_right_box_subject_main_box">
+                                    <div className="main_right_box_subject_main_box_left">
+                                        <div className="main_right_box_subject_main_box_left_head">
+                                            <div className="main_right_box_subject_main_box_left_head_circle"></div>
+                                        </div>
+                                        <i className="fa-regular fa-face-smile"></i>
+                                    </div>
+                                    <div className="main_right_box_subject_main_box_right">
+                                        <div className="main_right_box_subject_main_box_right_head">
+                                            Teacher
+                                        </div>
+                                        <div className="main_right_box_subject_main_box_right_main">
+                                            
+                                            <div className="main_right_box_subject_main_box_right_main_head">
+                                                <h3>Calculus 1  Pre Test - Drip yo yo</h3>
+                                                <p>ครบกำหนด 20 ต.ค.</p>
+                                            </div>
+                                            <div className="main_right_box_subject_main_box_right_main_main">
+                                                <p>Hey! Guys I want you to try this pre test first to check that you guys ready for study!</p>
+                                            </div>
+                                        </div>
+                                        <div className="main_right_box_subject_main_box_right_tail">
+                                            <div className="main_right_box_subject_main_box_right_tail_left">
+                                                <p>Add your reaction</p>
+                                            </div>
+                                            <div className="main_right_box_subject_main_box_right_tail_right">
+                                                <div className="main_right_box_subject_main_box_right_tail_right_score">
+                                                    <p>100 Score</p>
+                                                </div>
+                                                <div className="main_right_box_subject_main_box_right_tail_right_choice">
+                                                    <i className="fa-regular fa-pen-to-square"></i><p>Multiple Choice</p>
+                                                </div>
+                                                <div className="main_right_box_subject_main_box_right_tail_right_box">
+                                                    <a href="{% url 'take_test_page' %}">Take Test</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="main_right_box_subject_main">
+                                <div className="main_right_box_subject_main_box">
+                                    <div className="main_right_box_subject_main_box_left">
+                                        <div className="main_right_box_subject_main_box_left_head">
+                                            <div className="main_right_box_subject_main_box_left_head_circle"></div>
+                                        </div>
+                                        <i className="fa-regular fa-face-smile"></i>
+                                    </div>
+                                    <div className="main_right_box_subject_main_box_right">
+                                        <div className="main_right_box_subject_main_box_right_head">
+                                            Teacher
+                                        </div>
+                                        <div className="main_right_box_subject_main_box_right_main">
+                                            
+                                            <div className="main_right_box_subject_main_box_right_main_head">
+                                                <h3>Calculus 1  Pre Test - Drip yo yo</h3>
+                                                <p>ครบกำหนด 20 ต.ค.</p>
+                                            </div>
+                                            <div className="main_right_box_subject_main_box_right_main_main">
+                                                <p>Hey! Guys I want you to try this pre test first to check that you guys ready for study!</p>
+                                            </div>
+                                        </div>
+                                        <div className="main_right_box_subject_main_box_right_tail">
+                                            <div className="main_right_box_subject_main_box_right_tail_left">
+                                                <p>Add your reaction</p>
+                                            </div>
+                                            <div className="main_right_box_subject_main_box_right_tail_right">
+                                                <div className="main_right_box_subject_main_box_right_tail_right_score">
+                                                    <p>100 Score</p>
+                                                </div>
+                                                <div className="main_right_box_subject_main_box_right_tail_right_choice">
+                                                    <i className="fa-regular fa-pen-to-square"></i><p>Multiple Choice</p>
+                                                </div>
+                                                <div className="main_right_box_subject_main_box_right_tail_right_box">
+                                                    <a href="{% url 'take_test_page' %}">Take Test</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                             <div className="main_right_box_subject_main">
