912ccf00f1f5c697d15bf5d399dc1ceef92fdcf5
BigcBow <s6404062610481@email.kmutnb.ac.th>
2024-11-02 07:18:59 +0700

    add media css grid

--- QuizzesManagementFrontend/src/pages/teacher/Home_teacher.jsx
+++ QuizzesManagementFrontend/src/pages/teacher/Home_teacher.jsx
@@ -1,5 +1,5 @@
 import Navbar from "../student/navbar";
-import '../../styles/student/home.css';
+import '../../styles/teacher/home_teacher.css';
 import Navbar_top from "../student/Navbar_top";
 
 function Home_teacher() {
@@ -12,8 +12,106 @@ function Home_teacher() {
                     <Navbar_top/>
                 </div>
                 
-                <div className="main_home_right_main">
-                    
+                <div className="main_right_home_teacher">
+                    <div className="main_right_teacher_box_container">
+                        <div className="main_right_teacher_box">
+                            <div className="main_right_teacher_box_head">
+                                
+                            </div>
+                            <div className="main_right_teacher_box_tail">
+                                
+                            </div>
+                        </div>
+
+                        <div className="main_right_teacher_box">
+                            <div className="main_right_teacher_box_head">
+                                
+                            </div>
+                            <div className="main_right_teacher_box_tail">
+                                
+                            </div>
+                        </div>
+
+                        <div className="main_right_teacher_box">
+                            <div className="main_right_teacher_box_head">
+                                
+                            </div>
+                            <div className="main_right_teacher_box_tail">
+                                
+                            </div>
+                        </div>
+
+                        <div className="main_right_teacher_box">
+                            <div className="main_right_teacher_box_head">
+                                
+                            </div>
+                            <div className="main_right_teacher_box_tail">
+                                
+                            </div>
+                        </div>
+
+                        <div className="main_right_teacher_box">
+                            <div className="main_right_teacher_box_head">
+                                
+                            </div>
+                            <div className="main_right_teacher_box_tail">
+                                
+                            </div>
+                        </div>
+                        <div className="main_right_teacher_box">
+                            <div className="main_right_teacher_box_head">
+                                
+                            </div>
+                            <div className="main_right_teacher_box_tail">
+                                
+                            </div>
+                        </div>
+                        <div className="main_right_teacher_box">
+                            <div className="main_right_teacher_box_head">
+                                
+                            </div>
+                            <div className="main_right_teacher_box_tail">
+                                
+                            </div>
+                        </div>
+                        <div className="main_right_teacher_box">
+                            <div className="main_right_teacher_box_head">
+                                
+                            </div>
+                            <div className="main_right_teacher_box_tail">
+                                
+                            </div>
+                        </div>
+                        <div className="main_right_teacher_box">
+                            <div className="main_right_teacher_box_head">
+                                
+                            </div>
+                            <div className="main_right_teacher_box_tail">
+                                
+                            </div>
+                        </div>
+                        <div className="main_right_teacher_box">
+                            <div className="main_right_teacher_box_head">
+                                
+                            </div>
+                            <div className="main_right_teacher_box_tail">
+                                
+                            </div>
+                        </div>
+                        <div className="main_right_teacher_box">
+                            <div className="main_right_teacher_box_head">
+                                
+                            </div>
+                            <div className="main_right_teacher_box_tail">
+                                
+                            </div>
+                        </div>
+                        
+                    </div>
+
+                    <button className="box_add_subject">
+                        <i className="fa-solid fa-plus"></i>
+                    </button>
                 </div>
             </div>
         </div>
