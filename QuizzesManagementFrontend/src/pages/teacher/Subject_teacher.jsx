a5d56513426599916b53d84bf8b283f95802046a
BigcBow <s6404062610481@email.kmutnb.ac.th>
2024-11-02 18:02:26 +0700

    Create Subject Teacher Page

---
+++ QuizzesManagementFrontend/src/pages/teacher/Subject_teacher.jsx
@@ -0,0 +1,27 @@
+import Navbar from "../student/navbar";
+import '../../styles/teacher/home_teacher.css';
+import Navbar_top from "../student/Navbar_top";
+
+// import { useState } from "react";
+
+function Subject_teacher() {
+    return (
+        <div>
+            <div className="main_home">
+                <Navbar/>
+                <div className="main_home_right">
+                    <div className="main_home_right_top">
+                        <Navbar_top/>
+                    </div>
+                    
+                    <div className="main_right_home_teacher">
+                        
+                            
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Subject_teacher
