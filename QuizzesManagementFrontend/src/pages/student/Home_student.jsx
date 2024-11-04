462b4f6f9bf129014bf2fa76756a9a7c245c833c
BigcBow <s6404062610481@email.kmutnb.ac.th>
2024-11-05 06:20:55 +0700

    Update Home_student.jsx

--- QuizzesManagementFrontend/src/pages/student/Home_student.jsx
+++ QuizzesManagementFrontend/src/pages/student/Home_student.jsx
@@ -6,6 +6,7 @@ import { useNavigate, useLocation, Link } from "react-router-dom";
 import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBook, faPlus } from '@fortawesome/free-solid-svg-icons';
+import Swal from 'sweetalert2'
 
 export default function Home_student() {
     const navigate = useNavigate();
@@ -15,6 +16,10 @@ export default function Home_student() {
     const user_id = sessionStorage.getItem('user_id'); // ดึง user_id จาก sessionStorage
     const [subjects, setSubjects] = useState('');
     const [error, setError] = useState('');
+    const [modalSubject, setModalSubject] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+    const [successMessage, setSuccessMessage] = useState('');
+    const [numsubject, setNumsubject] = useState('');
 
     useEffect(() => {
         const fetchSubjects = async () => {
@@ -36,6 +41,19 @@ export default function Home_student() {
         navigate(`/subject_student/${code}`);
     };
 
+    const toggleModalSubject = () => {
+        setModalSubject(!modalSubject)
+    }
+
+    const toggleSuccessModal = () => {
+        Swal.fire({
+            icon: "success",
+            title: "Your work has been saved",
+            showConfirmButton: false,
+            timer: 2500
+          });
+        // setShowSuccessModal(!showSuccessModal);
+    };
 
 
     return (
@@ -47,12 +65,6 @@ export default function Home_student() {
                 </div>
                 
                 <div className="main_right_student_box_container">
-
-                    <div className="add_subject_student_container">
-                        <button className="add_subject_student_box">
-                            <FontAwesomeIcon icon={faPlus} />
-                        </button>
-                    </div>
                     
                     {subjects.length > 0 ? (
                         subjects.map(subject => (
@@ -78,6 +90,52 @@ export default function Home_student() {
                         </div>
                     )}
                 </div>
+
+                <div className="main_right_home_teacher">
+
+                    <div className="main_right_teacher_box_container">
+
+                    </div>
+                    <button className="box_add_subject">
+                        <FontAwesomeIcon icon={faPlus} className="plus" onClick={toggleModalSubject} />
+                    </button>
+                </div>
+
+                {modalSubject && (
+                    <div className="popup_container">
+                        <div className="popup_container_box">
+                            <div className="popup_box">
+                                <form method="POST">
+                                    {/* {% csrf_token %} */}
+                                    <div className="popup_box_top">
+                                        <div className="popup_box_top_left">
+                                            <label className="popup_box_top_left_num">รหัสวิชา :</label>
+                                        </div>
+                                        <div className="popup_box_top_right">
+                                            <div className="popup_box_top_right_input_name">
+                                                <input type="text" name="name"
+                                                    value={numsubject}
+                                                    onChange={(e) => setNumsubject(e.target.value)}
+                                                />
+                                                {/* {{ form.name }} */}
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="popup_box_tail">
+                                        <button type="button" className="popup_box_tail_cancel" onClick={toggleModalSubject}>Cancel</button>
+                                        <button
+                                            type="submit"
+                                            className="popup_box_tail_save"
+                                            // onClick={handleSubmit}
+                                        >Add</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                )
+                }
+
             </div>
         </div>
     );
